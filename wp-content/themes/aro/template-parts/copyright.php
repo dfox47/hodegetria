e06ac66d1ac5cc2c1daa266b272db0292a8bbd57
dfox <dfox@foxartbox.com>
2023-01-15 17:16:43 +0200

    copyright refactoring

--- wp-content/themes/aro/template-parts/copyright.php
+++ wp-content/themes/aro/template-parts/copyright.php
@@ -1,6 +1,6 @@
 <div class="copyright-bar">
 	<div class="container">
-		<div class="row  align-items-center">
+		<div class="row align-items-center">
 			<div class="column-12">
 				<?php aro_credit(); ?>
 			</div>
