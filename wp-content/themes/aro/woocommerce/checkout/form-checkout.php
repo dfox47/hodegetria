50f4b545e125fff59b96f3be0ab2b73e17d84a88
dfox <dfox@foxartbox.com>
2023-02-08 21:32:22 +0200

    Cheackout. Translate title

--- wp-content/themes/aro/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/aro/woocommerce/checkout/form-checkout.php
@@ -25,14 +25,10 @@ do_action( 'woocommerce_before_checkout_form', $checkout );
 if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
 	echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', esc_html__( 'You must be logged in to checkout.', 'aro' ) ) );
 	return;
-}
-
-?>
+} ?>
 
 <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
-
 	<?php if ( $checkout->get_checkout_fields() ) : ?>
-
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
 		<div class="col2-set" id="customer_details">
@@ -46,19 +42,19 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 		</div>
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
-
 	<?php endif; ?>
 
-    <?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
+	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
-    <div id="order_review" class="woocommerce-checkout-review-order">
-	    <?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
-        <h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'aro' ); ?></h3>
-        <?php do_action( 'woocommerce_checkout_order_review' ); ?>
+	<div id="order_review" class="woocommerce-checkout-review-order">
+		<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
+
+		<h3 id="order_review_heading">Вашата поръчка</h3>
+
+		<?php do_action( 'woocommerce_checkout_order_review' ); ?>
 	</div>
 
 	<?php do_action( 'woocommerce_checkout_after_order_review' ); ?>
-
 </form>
 
 <?php do_action( 'woocommerce_after_checkout_form', $checkout ); ?>
