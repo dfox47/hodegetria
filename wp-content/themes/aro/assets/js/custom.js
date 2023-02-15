586e862a63fcf1c08f0cdcdbd5f7a7ad21ed4e01
dfox <dfox@foxartbox.com>
2023-02-15 14:55:00 +0200

    Subscribe. Check & hide agree.

--- wp-content/themes/aro/assets/js/custom.js
+++ wp-content/themes/aro/assets/js/custom.js
@@ -1,5 +1,11 @@
 console.log('custom.js loading...')
 
+document.querySelectorAll('input[name="AGREE_TO_TERMS"]').forEach((e) => {
+	e.checked = true
+})
 
+document.querySelectorAll('.mc4wp-form-fields').forEach((e) => {
+	e.querySelector('.form-line').classList.add('hidden')
+})
 
 console.log('custom.js loaded')
